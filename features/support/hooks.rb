0f027bff6c22968a08ad061b2c0d049f2af06f64
Andre Luiz Pelicce Albino <andre.albino@sp01.local>
2019-06-18 11:41:31 -0300

    ajustando projeto9

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -2,13 +2,13 @@ Before do
   @login_page = LoginPage.new
   @movie_page = MoviePage.new
   @sidebar = SideBarView.new
-
   page.current_window.resize_to(1920, 1080)
+  
 end
 
 Before("@login") do
   user = CONFIG["users"]["cat_manager"]
   @login_page.go
-  @login_page.with(user["email"], user["pass"])
+  @login_page.with(user["email"],user["pass"])
 end
 
